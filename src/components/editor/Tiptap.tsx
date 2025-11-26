275e7966b05919d9ca8f86fffec3bd3efd4432b5
[dyad] <rodrigopepe281@gmail.com>
2025-11-26 12:34:38 -0400

    [dyad] Fixed an import error in the document editor - wrote 1 file(s)

--- src/components/editor/Tiptap.tsx
+++ src/components/editor/Tiptap.tsx
@@ -4,7 +4,7 @@ import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import { Toolbar } from './Toolbar';
 import { Color } from '@tiptap/extension-color'
-import TextStyle from '@tiptap/extension-text-style'
+import { TextStyle } from '@tiptap/extension-text-style'
 
 const Tiptap = ({
   content,
